aae3f1ffa1c22f78a31b433ffe0c51c143445471
jibon49 <imoinul749@gmail.com>
2023-12-25 21:20:16 +0600

    update my task on dnd

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const { ObjectId } = require('mongodb');
+
 
 app.use(cors());
 app.use(express.json())
@@ -48,6 +50,48 @@ async function run() {
 
 
 
+        //   task related
+        app.post('/create-task', async (req, res) => {
+            const task = req.body
+            const result = await taskCollection.insertOne(task)
+            res.send(result);
+          })
+
+          app.get('/my-task', async (req, res) => {
+            let query = {}
+      
+            if (req.query?.email) {
+              query = {
+                'author.email': req.query.email,
+              }
+              const result = await taskCollection.find(query).toArray();
+              res.send(result)
+            }
+      
+          })
+
+          app.patch('/update-task-status/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id,req.body)
+            const filter = { _id: new ObjectId(id) };
+            const updateStatus = req.body;
+            console.log(updateStatus.status)
+          
+            const updatedDoc = {
+              $set: {
+                status : updateStatus.status
+              },
+          };
+          const result = await taskCollection.updateOne(filter,updatedDoc)
+          res.send(result);
+          });
+          
+
+
+          app.get('/task',async(req,res)=>{
+            const result = await taskCollection.find().toArray();
+            res.send(result)
+          })
 
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
